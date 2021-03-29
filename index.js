3756408f85a6f4efd723405aa53b6daa3cdce630
haven <haven.eldebaque@gmail.com>
2021-03-29 20:40:13 +1000

    update2

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
 
 
-// function addEventListner() {
-//     const submit = document.getElementsByClassName('submit');
-//     submit.addEventListener('click', function (event) {
-//         alert('Your details are submitted!')
-//     });
-// }
+// // function addEventListner() {
+// //     const submit = document.getElementsByClassName('submit');
+// //     submit.addEventListener('click', function (event) {
+// //         alert('Your details are submitted!')
+// //     });
+// // }
 
-const dictionaryForm = document.getElementsById(dictionaryForm);
+// // document.querySelector(".submit").addEventListener('click', function (event) {
+// //     alert('Your details are submitted')
+// // });
+// // addEventListener();
 
-dictionaryForm.addEventListener('submit', fetchServer());
 
-function fetchServer() {
-    const definition = document.getElementsById('definition');
-    const symbols = document.getElementsById('symbols');
-    const spelling = document.getElementsById('spelling');
+
+const newsForm = document.getElementById('newsForm');
+
+newsForm.addEventListener('submit', fetchServer);
+
+function fetchServer(event) {
+    event.preventDefault()
+    const main = document.querySelector('main'); 
+    main.insertAdjacentElement('afterend', '<p id="display"></p>'); 
+    const dateFrom = document.getElementById('date_from');
+    const dateTo = document.getElementById('date_to');
+    const domains = document.getElementById('domains');
+    const keyword = document.getElementById('keyword');
+    const newTerm = {
+        dateFrom: dateFrom,
+        dateTo: dateTo,
+        domains: domains,
+    };
+    console.log(newTerm.dateFrom, newTerm.dateTo, newTerm.domains);
+
+    const configurationObject = {
+        method: "GET",
+        headers: {
+            "Accept": "application/json"    
+        },
+
+    };
+
+    fetch(`https://newsapi.org/v2/everything?q=${keyword.value}&apiKey=e71137b4bffe4e62a640d13070e6536d`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+        });
 }
 
-document.querySelector(".submit").addEventListener('click', function (event) {
-    alert('Your details are submitted')
-});
 
-addEventListener();
+// console.log()
+// data.innerHTML(data.sourceId, data.sourceName, data.sourceAuthor, data.sourceTitle); 
 
-configurationObject = {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        dogName: "Byron",
-        dogBreed: "Poodle"
-    })
-};
-};
+// fetch(`https://newsapi.org/v2/everything?q=doctor&apiKey=e71137b4bffe4e62a640d13070e6536d`)
+//     .then(function (response) {
+//         return response.json();
+//     }).then(function (data) {
+//         console.log(data);
+//     });
 
-fetch('https://www.dictionaryapi.com/api/v3/references/medical/json/doctor?key=your-api-key')
-    .then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        console.log(json);
-        console.log(`Holy cow! There are ${json["number"]} humans in space.`);
-    });
+// Albert Notes:
+// change (json) to (data) in rows 53, 54 
+// back ticks `` is used for the url in row 50 NOT double quotation 
