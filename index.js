f71a625f0124dc4b59c42f4cac0736e0e26c7187
haven <haven.eldebaque@gmail.com>
2021-04-30 14:24:09 +1000

    Initial style pushed to update3

--- index.js
+++ index.js
@@ -1,65 +1,72 @@
 
-
-// // function addEventListner() {
-// //     const submit = document.getElementsByClassName('submit');
-// //     submit.addEventListener('click', function (event) {
-// //         alert('Your details are submitted!')
-// //     });
-// // }
-
-// // document.querySelector(".submit").addEventListener('click', function (event) {
-// //     alert('Your details are submitted')
-// // });
-// // addEventListener();
-
-
-
 const newsForm = document.getElementById('newsForm');
+const display = document.getElementById('display');
+const submitbutton = document.getElementById('submitbutton');
+const searchResult = document.getElementById('searchResult');
+const clear = document.getElementById('clear');
+submitbutton.addEventListener('click', function (e) {
+    e.preventDefault();
+    fetchArticles();
+    // console.log("event is prevented")
+});
 
-newsForm.addEventListener('submit', fetchServer);
+const configurationObject = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json"
+    },
 
-function fetchServer(event) {
-    event.preventDefault()
-    const main = document.querySelector('main'); 
-    main.insertAdjacentElement('afterend', '<p id="display"></p>'); 
-    const dateFrom = document.getElementById('date_from');
-    const dateTo = document.getElementById('date_to');
-    const domains = document.getElementById('domains');
-    const keyword = document.getElementById('keyword');
-    const newTerm = {
-        dateFrom: dateFrom,
-        dateTo: dateTo,
-        domains: domains,
-    };
-    console.log(newTerm.dateFrom, newTerm.dateTo, newTerm.domains);
+};
+// console.log("GET is working")
 
-    const configurationObject = {
-        method: "GET",
-        headers: {
-            "Accept": "application/json"    
-        },
+clear.addEventListener('click', e => {
+    clearChildren()
+})
+function clearChildren() {
+    searchResult.innerHTML = ''
+}
 
-    };
-
+function fetchArticles() {
     fetch(`https://newsapi.org/v2/everything?q=${keyword.value}&apiKey=e71137b4bffe4e62a640d13070e6536d`)
         .then(function (response) {
             return response.json();
         }).then(function (data) {
-            console.log(data);
-        });
+            console.log(data.articles);
+            // clearChildren()
+            const html = data.articles.map(article => {
+                console.log("articles", article)
+                const listItem = document.createElement('li');
+                listItem.innerHTML =
+                    `<div id="displayedDetails">
+            <div class="artImage"> <img class="articleImage" src="${article.urlToImage}"</div> 
+            <div class="artTitle"> <strong>Article Title:</strong> ${article.title}. </div> 
+            <div class="artAuthor"> <strong>Puplished by:</strong> ${article.author}</div>
+            <div> <a href="${article.url}" class="artView" <strong> View Article </strong> </a>  </div>
+            </div>`;
+
+            // <div class="artContent"><strong>Article Content:</strong> ${article.content}</div>
+            // <div class="artUrl"><strong>Article url:</strong> ${article.url}</div>
+
+                // const articleInfo = document.createElement('b');
+                // listItem.append(articleInfo);
+
+                searchResult.append(listItem)
+            });
+            console.log(html);
+        })
+
+
+
+    // for (let i = 0; i < article.length; i++) {
+    //     const keywordResult = document.createElement('li');
+    //     const articleInfo = document.createElement('b');
+    //     articleInfo.append(articles.author.value)
+    //     articleInfo.append(title.value)
+    //     articleInfo.append(url.value)
+    //     articleInfo.append(source.name)
+    //     articleInfo.append(publishedAt.value)
+    //     keywordResult.append(articleInfo);
+
+    // }
 }
 
-
-// console.log()
-// data.innerHTML(data.sourceId, data.sourceName, data.sourceAuthor, data.sourceTitle); 
-
-// fetch(`https://newsapi.org/v2/everything?q=doctor&apiKey=e71137b4bffe4e62a640d13070e6536d`)
-//     .then(function (response) {
-//         return response.json();
-//     }).then(function (data) {
-//         console.log(data);
-//     });
-
-// Albert Notes:
-// change (json) to (data) in rows 53, 54 
-// back ticks `` is used for the url in row 50 NOT double quotation 
